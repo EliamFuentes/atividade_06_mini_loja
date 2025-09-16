231b5272ae71147b271d2f79c404b9a364e46db5
EliamFuentes <eliamfuentes123@gmail.com>
2025-09-16 11:18:04 -0300

    Adicionando arquivos na pasta 04-styled-components

---
+++ src/04-styled-components/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Store, ShoppingCart } from "lucide-react";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
+
+export default function Navbar({ toggleTheme, theme, cartCount }) {
+
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <Store />
+                <span>Mini Loja</span>
+            </div>
+
+            <div className="navbar-actions">
+                <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+                <div className="cart">
+                    <ShoppingCart />
+                    <span className="cart-count">{cartCount}</span>
+                </div>
+            </div>
+        </nav>
+    );
+}
